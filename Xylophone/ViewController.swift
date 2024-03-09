eb8896579f54be3446397417e5dd4f01a8481e2e
pravinrajpaul <pravinrajpaul@gmail.com>
2024-03-09 23:19:00 +0530

    Sound Files Added and Code to Play Sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func CButton(_ sender: UIButton) {
+        playSound(fileName:"C")
+    }
+    
+    @IBAction func DButton(_ sender: UIButton) {
+        playSound(fileName:"D")
+    }
+    
+    @IBAction func EButton(_ sender: UIButton) {
+        playSound(fileName:"E")
+    }
+    
+    @IBAction func FButton(_ sender: UIButton) {
+        playSound(fileName:"F")
+    }
+    
+    @IBAction func GButton(_ sender: UIButton) {
+        playSound(fileName:"G")
+    }
+    
+    @IBAction func AButton(_ sender: UIButton) {
+        playSound(fileName:"A")
+    }
+        
+    @IBAction func BButton(_ sender: UIButton) {
+        playSound(fileName:"B")
+    }
+    
+    func playSound(fileName:String) {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "wav") else
+        {
+            print("Unable to play the sound for \(fileName)")
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else {return}
+            player.play()
+            print("Played \(fileName)")
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
